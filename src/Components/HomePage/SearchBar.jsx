bef9878301e0ab52834eca84e5905d9926c072b8
asrar <aliasrar84@gmail.com>
2023-11-29 14:14:58 +0530

    new

--- src/Components/HomePage/SearchBar.jsx
+++ src/Components/HomePage/SearchBar.jsx
@@ -1,29 +1,29 @@
-import React, { useState } from 'react'
-import { FaSearch } from "react-icons/fa";
+// import React, { useState } from 'react'
+// import { FaSearch } from "react-icons/fa";
 
-const SearchBar = () => {
-    const [search, setSearch] = useState("")
+// const SearchBar = () => {
+//     const [search, setSearch] = useState("")
 
-    const handleChange = (e) => {
-        setSearch(e.target.value)
-    }
+//     const handleChange = (e) => {
+//         setSearch(e.target.value)
+//     }
 
-  return (
-    <div>
-        <div className='flex justify-center items-center mt-5 '>
-        <input 
-        className=' search text-lg px-2 py-1  outline-none mx-5'
-        type="text"
-        placeholder='Search Here'
-        onChange={handleChange}
-        value={search}
-        /> 
-        <div className='bg-slate-200 p-2 rounded-full text-slate-900 cursor-pointer'>
-        <FaSearch />
-        </div>
-        </div>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//         <div className='flex justify-center items-center mt-5 '>
+//         <input 
+//         className=' search text-lg px-2 py-1  outline-none mx-5'
+//         type="text"
+//         placeholder='Search Here'
+//         onChange={handleChange}
+//         value={search}
+//         /> 
+//         <div className='bg-slate-200 p-2 rounded-full text-slate-900 cursor-pointer'>
+//         <FaSearch />
+//         </div>
+//         </div>
+//     </div>
+//   )
+// }
 
-export default SearchBar
+// export default SearchBar
